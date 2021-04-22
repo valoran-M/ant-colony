023788d8bd170c8185292044733d3ef661ceed4f
Valeran-M <v.maytie85@orange.fr>
2021-04-22 14:36:28 +0200

    aff getNeigbour in coordinate

--- src/models/coordinate.cpp
+++ src/models/coordinate.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <vector>
 
 #include "models/coordinate.hpp"
 
@@ -25,4 +26,20 @@ bool Coord::operator==(Coord const &other) const
 bool Coord::operator!=(Coord const &other) const
 {
     return _x != other.getX() || _y != other.getY();
+}
+
+std::vector<Coord> Coord::getNeigbour(std::size_t width, std::size_t height) const
+{
+    std::size_t xMin = std::max<std::size_t>(getX() - 1, 0);
+    std::size_t xMax = std::min<std::size_t>(getX() + 1, 0);
+    std::size_t yMin = std::max<std::size_t>(getY() - 1, 0);
+    std::size_t yMax = std::min<std::size_t>(getY() + 1, 0);
+
+    std::vector<Coord> neigbours;
+
+    for (std::size_t y = yMin; y < yMax; y++)
+        for (std::size_t x = xMin - 1; x < xMax; x++)
+            if (x != getX() && y != getY())
+                neigbours.push_back(Coord(x, y));
+    return neigbours;
 }
