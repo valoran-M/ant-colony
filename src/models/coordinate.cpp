2cf358932cafed2a7e4d3f94cb0a9bfa361b0cae
Valeran-M <v.maytie85@orange.fr>
2021-04-22 14:19:16 +0200

    add coordinate operator

--- src/models/coordinate.cpp
+++ src/models/coordinate.cpp
@@ -1,6 +1,28 @@
+#include <stdexcept>
+
 #include "models/coordinate.hpp"
 
 Coord::Coord(std::size_t x, std::size_t y) : _x(x),
                                              _y(y)
 {
+}
+
+std::size_t &Coord::operator[](unsigned i)
+{
+    if (i == 0)
+        return _x;
+    else if (i == 1)
+        return _y;
+    else
+        throw std::invalid_argument("The coordinates are in 2 dimension");
+}
+
+bool Coord::operator==(Coord const &other) const
+{
+    return _x == other.getX() && _y == other.getY();
+}
+
+bool Coord::operator!=(Coord const &other) const
+{
+    return _x != other.getX() || _y != other.getY();
 }
--- src/models/coordinate.hpp
+++ src/models/coordinate.hpp
@@ -8,8 +8,10 @@ private:
     std::size_t _y;
 
 public:
-    int &operator[](unsigned i);
-    bool operator==(Coord const & other);
-    bool operator!=(Coord const & other);
+    std::size_t &operator[](unsigned i);
+    std::size_t getX() const { return _x; }
+    std::size_t getY() const { return _y; }
+    bool operator==(Coord const &other) const;
+    bool operator!=(Coord const &other) const;
     Coord(std::size_t x, std::size_t y);
 };
